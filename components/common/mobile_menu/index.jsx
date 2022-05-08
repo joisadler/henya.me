521c936236254f7753404647eab0fa3927452895
Yossi Adler <joisadler@gmail.com>
2022-05-08 09:58:35 +0300

    add social links to mobile menu

--- components/common/mobile_menu/index.jsx
+++ components/common/mobile_menu/index.jsx
@@ -2,14 +2,18 @@ import { useEffect, useRef, useContext } from 'react';
 import Context from 'context/Context';
 import { arrayOf, shape, string, number } from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import styles from './mobile_menu.module.scss';
 import MobileNavLink from './MobileNavLink';
 
 const MobileMenu = ({ nav_links }) => {
-  const { isMobileMenuOpen, showMobileMenu, hideMobileMenu } =
-    useContext(Context);
+  const { isMobileMenuOpen, hideMobileMenu } = useContext(Context);
   const menuRef = useRef(null);
+
+  const onMenuClick = () => {
+    hideMobileMenu();
+  };
 
   useEffect(() => {
     const menu = menuRef.current;
@@ -22,7 +26,12 @@ const MobileMenu = ({ nav_links }) => {
   }, [isMobileMenuOpen]);
 
   return (
-    <div className={styles.container} ref={menuRef}>
+    <div
+      className={styles.container}
+      ref={menuRef}
+      onClick={onMenuClick}
+      role="presentation"
+    >
       <nav className={styles.mobile_nav}>
         {nav_links.map(({ id, ...linkProps }) => (
           <MobileNavLink {...linkProps} />
@@ -36,6 +45,22 @@ const MobileMenu = ({ nav_links }) => {
           rel="noopener noreferrer"
         >
           <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+        <a
+          className={styles.contact_link}
+          href="mailto:henyadesign@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+        <a
+          className={styles.contact_link}
+          href="https://www.linkedin.com/in/henyaadler/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
       </nav>
     </div>
