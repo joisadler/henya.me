ac1c8f32b35e758bcf5add29805a5113429d9a6a
Yossi Adler <joisadler@gmail.com>
2022-05-22 15:37:56 +0300

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,28 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         {/* example for prefetch images: */}
         {/* <link rel="prefetch" href="/images/image.png" /> */}
 
