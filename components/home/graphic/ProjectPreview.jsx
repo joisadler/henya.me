47131e281b38766f57e48f73cd74d358007a4458
Yossi Adler <joisadler@gmail.com>
2022-05-23 15:14:30 +0300

    rename Graphic to graphic

---
+++ components/home/graphic/ProjectPreview.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+import { string } from 'prop-types';
+import styles from './graphic.module.scss';
+
+const UxUiProjectPreview = ({ preview_image_filename, name }) => {
+  return (
+    <li className={styles.project_preview}>
+      <picture className={styles.project_preview_picture}>
+        <source srcSet={`${preview_image_filename}.webp`} type="image/webp" />
+        <source srcSet={`${preview_image_filename}.png`} type="image/png" />
+        <img
+          className={styles.project_preview_image}
+          src={`${preview_image_filename}.webp`}
+          alt={name}
+        />
+      </picture>
+    </li>
+  );
+};
+
+UxUiProjectPreview.propTypes = {
+  preview_image_filename: string.isRequired,
+  name: string.isRequired,
+};
+
+export default UxUiProjectPreview;
---
+++ components/home/graphic/index.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { arrayOf, shape, number, string } from 'prop-types';
+import RightArrowIcon from 'components/icons/RightArrowIcon';
+import ProjectPreview from './ProjectPreview';
+import styles from './graphic.module.scss';
+
+const Graphic = ({ projects }) => {
+  const [isAllItemsShown, setIsAllItemsShown] = useState(false);
+
+  const onShowMoreClick = () => {
+    setIsAllItemsShown(true);
+  };
+
+  const itemsToShow = (() => {
+    if (isAllItemsShown) return projects;
+    return projects.slice(0, 4);
+  })();
+
+  return (
+    <section id="graphic" className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.section_title}>Graphic Design</h2>
+        <ul className={styles.projects_list}>
+          {itemsToShow.map((project) => {
+            const { id } = project;
+            return <ProjectPreview key={id} {...project} />;
+          })}
+        </ul>
+        {!isAllItemsShown && (
+          <div
+            className={styles.view_more_button}
+            onClick={onShowMoreClick}
+            role="presentation"
+          >
+            <span className={styles.view_more_button_text}>View More</span>
+            <RightArrowIcon className={styles.right_arrow_icon} color="#fff" />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+Graphic.propTypes = {
+  projects: arrayOf(
+    shape({
+      id: number.isRequired,
+      preview_image_filename: string.isRequired,
+      name: string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Graphic;
