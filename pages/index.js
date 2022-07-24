3155e02430d2242fc416b5071d895506f30ff72e
Yossi Adler <joisadler@gmail.com>
2022-07-25 00:28:02 +0300

    fix a bug with adding user

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ const Home = ({
   const mmmEasterEgg = (e) => {
     if (e.key === 'm') {
       pressCountRef.current++;
-      console.log('pressCountRef.current', pressCountRef.current);
       if (pressCountRef.current === 3) {
         console.log('\n');
         pressCountRef.current = 0;
