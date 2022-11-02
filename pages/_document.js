8f999004ccb1a2f04823724518d5d0ff87e41fae
Yossi Adler <joisadler@gmail.com>
2022-11-02 17:14:26 +0200

    try to add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   // Make scroll smooth
@@ -55,6 +56,22 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Alata&family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
       </Head>
       <body>
         <Main />
