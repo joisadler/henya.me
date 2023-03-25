0be2d78dbd6603c4773b4e3a69c1d74063179db1
Yossi Adler <joisadler@gmail.com>
2023-03-25 20:28:25 +0300

    add motion projects

---
+++ components/home/motion/MotionProjectPreview.jsx
@@ -0,0 +1,55 @@
+import { useRef } from 'react';
+import { useFullscreen } from 'rooks';
+import { string, func } from 'prop-types';
+import { tablet_breakpoint } from 'config/constants';
+import { useMediaQuery } from 'react-responsive';
+import WebpPicture from 'components/common/WebpPicture';
+import styles from './motion.module.scss';
+
+const GraphicProjectPreview = ({ media_filename, name, openModal }) => {
+  const isMobile = useMediaQuery({
+    maxWidth: tablet_breakpoint - 1,
+  });
+  const fullscreenContainerRef = useRef(null);
+  const { isFullscreenAvailable, isFullscreenEnabled, toggleFullscreen } =
+    useFullscreen({
+      target: fullscreenContainerRef,
+    });
+
+  const onPreviewButtonClick = () => {
+    if (isMobile && isFullscreenAvailable) {
+      toggleFullscreen();
+    } else {
+      openModal({ name, media_filename });
+    }
+  };
+
+  return (
+    <li className={styles.project_preview}>
+      <WebpPicture
+        ref={fullscreenContainerRef}
+        containerClassName={styles.project_preview_picture}
+        imgClassName={styles.project_preview_image}
+        pathname="/videos/portfolio/motion/"
+        filename={`${media_filename}_preview`}
+        alt={name}
+        data-fullscreen-enabled={isFullscreenEnabled}
+      />
+      <button
+        type="button"
+        onClick={onPreviewButtonClick}
+        className={styles.project_preview_button}
+      >
+        <h3 className={styles.project_name}>{name}</h3>
+      </button>
+    </li>
+  );
+};
+
+GraphicProjectPreview.propTypes = {
+  media_filename: string.isRequired,
+  name: string.isRequired,
+  openModal: func.isRequired,
+};
+
+export default GraphicProjectPreview;
---
+++ data/motion_design_projects.data.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Carolina Lemke Invitation - story',
+    media_filename: 'carolina_lemke_invitation',
+  },
+  {
+    id: 2,
+    name: 'URBANICA Vacay Mode - story',
+    media_filename: 'urbanica',
+  },
+  {
+    id: 3,
+    name: 'KIKO Belezza Collection - story',
+    media_filename: 'kiko_belezza',
+  },
+  {
+    id: 4,
+    name: 'HOODIES Kids Final Sale - story',
+    media_filename: 'hoodies_kids',
+  },
+  {
+    id: 5,
+    name: 'HOODIES Summer Clearance - story',
+    media_filename: 'hoodies',
+  },
+  {
+    id: 6,
+    name: 'KIKO Birthday - story',
+    media_filename: 'kiko_birthday',
+  },
+  {
+    id: 7,
+    name: 'KIKO 50% Off - story',
+    media_filename: 'kiko',
+  },
+  {
+    id: 8,
+    name: 'Crypto - advertisement',
+    media_filename: 'crypto',
+  },
+];
