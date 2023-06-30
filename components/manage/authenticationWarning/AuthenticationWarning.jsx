dc9d2e6685a58f51ba10f485a81e070da390afb4
Yossi Adler <joisadler@gmail.com>
2023-06-30 18:35:33 +0300

    Delete components/manage/authenticationWarning directory

--- components/manage/authenticationWarning/AuthenticationWarning.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './authentication_warning.module.scss';
-
-const AuthenticationWarning = () => {
-  return <main className={styles.container} />;
-};
-
-export default AuthenticationWarning;
